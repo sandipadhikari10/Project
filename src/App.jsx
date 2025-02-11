69e210fcbb250632151e2cfef03d5b658cac2494
SANDIP ADHIKARI <sawndeep123@gmail.com>
2025-02-11 22:31:28 +0545

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
         <Route path="/Past-President" element={<PastPresident />} />
         <Route path="/RCC-West-Coast-Region" element={<WestCoast />} />
       </Routes>
-      <Footer2 />
+      <Footer />
     </>
   );
 }
